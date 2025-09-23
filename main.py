4510305f123b7c741a3bb6e1cea9e1584e28b1aa
Facundo <fac.demarco37@gmail.com>
2025-09-23 16:37:47 -0300

    Add main

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from routers import quintas
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(root_path="/MdpuF8KsXiRArNlHtl6pXO2XyLSJMTQ8_Zonaquintas/api")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://zonaquintas.com",
+    "https://www.zonaquintas.com",
+    "http://zonaquintas.com",
+    "http://www.zonaquintas.com",
+]
+
+app.add_middleware( 
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Set-Cookie"],
+)
+
+IMAGES_DIR = os.path.join(os.getcwd(), "images")
+app.mount("/MdpuF8KsXiRArNlHtl6pXO2XyLSJMTQ8_Zonaquintas/api/images", StaticFiles(directory=IMAGES_DIR), name="images")
+
+IS_PROD = os.getenv("ENV") == "production"
+print(IS_PROD)
+
+@app.get("/")
+async def root():
+    return {"message": "API Zona Quintas by iWeb Technology. 2025 All rights reserved."}
+
+app.include_router(quintas.router, prefix="/quintas", tags=["quintas"])
